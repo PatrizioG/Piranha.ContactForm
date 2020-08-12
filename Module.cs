1f9d989bc7200c6bf7d95e9692bd6997f5ee9de2
Patrizio Gasperi <patrizio.gasperi@gmail.com>
2020-08-12 09:39:06 +0200

    Add files via upload

---
+++ Module.cs
@@ -0,0 +1,27 @@
+﻿using Piranha.Extend;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Piranha.ContactForm
+{
+    public class Module : IModule
+    {
+        public string Author => "Gasperi Patrizio";
+
+        public string Name => "Piranha.ContactForm";
+
+        public string Version => Piranha.Utils.GetAssemblyVersion(GetType().Assembly);
+
+        public string Description => "";
+
+        public string PackageUrl => "";
+
+        public string IconUrl => "";
+
+        public void Init()
+        {
+
+        }
+    }
+}
---
+++ ModuleExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Piranha.ContactForm
+{
+    public static class ModuleExtensions
+    {
+        public static IServiceCollection AddContactFormModule(this IServiceCollection services)
+        {
+            App.Modules.Register<Module>();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseContactFormModule(this IApplicationBuilder builder)
+        {
+
+            return builder;
+        }
+    }
+}
